1bdfa98c6ef9f4b04e9cf1607b9c8048a8e639f3
admahmoo <adds.mahmood786@gmail.com>
2020-08-19 10:33:13 +0100

    Inital Commit

---
+++ src/main/java/com/globomatics/bikeApp/BikeAppApplication.java
@@ -0,0 +1,13 @@
+package com.globomatics.bikeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globomatics/bikeApp/BikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globomatics.bikeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
